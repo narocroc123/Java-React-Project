ba639a7a23945f101990301d9d02cd35c382d36c
Fergus Corcoran <fergus.corcoran@gmail.com>
2020-04-16 14:45:52 +0100

    reversed changes to package.json

--- src/main/java/com/makersacademy/acebook/controller/HomeController.java
+++ src/main/java/com/makersacademy/acebook/controller/HomeController.java
@@ -1,11 +1,7 @@
 package com.makersacademy.acebook.controller;
 
-import com.makersacademy.acebook.model.Post;
-import com.makersacademy.acebook.repository.PostRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HomeController {
