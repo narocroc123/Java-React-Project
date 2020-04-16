52d3b78aceacf23bd2edc55b8027ab75ca3ba43f
Fergus Corcoran <fergus.corcoran@gmail.com>
2020-04-16 10:59:06 +0100

    added structure for home controller

--- src/main/java/com/makersacademy/acebook/controller/HomeController.java
+++ src/main/java/com/makersacademy/acebook/controller/HomeController.java
@@ -1,14 +1,33 @@
 package com.makersacademy.acebook.controller;
 
+import com.makersacademy.acebook.model.Post;
+import com.makersacademy.acebook.repository.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HomeController {
+
+	@Autowired
+	private PostRepository postRepository;
+
+	@Autowired
+	public HomeController(PostRepository postRepository){
+		this.postRepository = postRepository;
+	}
 
 	@RequestMapping(value = "/")
 	public String index() {
 		return "index";
 	}
 
+	@RequestMapping(value = "api/createPost", method = RequestMethod.POST) {
+		public String createPost(@RequestParams String content) {
+			Post post = new Post(content);
+			postRepository.save(post);
+			return "redirect: index";
+		}
+	}
 }
---
+++ src/main/js/login/login.js
@@ -0,0 +1,16 @@
+import React from 'react';
+const client = require('../client');
+
+class Login extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return(
+            <h1>Login Page</h1>
+        );
+    }
+}
+
+export default Login;
